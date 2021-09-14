ce3fecde45ff4225d956379b3bb9ad4068f376aa
brunoJorquera <jorquera.b18@gmail.com>
2021-09-14 15:37:12 -0700

    Done

---
+++ server/controller.js
@@ -0,0 +1,47 @@
+const houses = require('./db.json')
+let globalId = 4
+
+module.exports = {
+    getHouses: (req,res) => {
+        res.status(200).send(houses)
+    },
+    deleteHouses: (req,res) => {
+        const {id} = req.params
+        let index = houses.findIndex(elem => elem.id === +id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    },
+    createHouse: (req,res) => {
+        const {address, price, imageURL} = req.body
+        let newHouse = {
+            id: globalId,
+            address,
+            price,
+            imageURL
+        }
+        if(!address || !price || !imageURL){
+            return res.status(400).send('Missing field, fill in all info')
+        } else{
+            houses.push(newHouse)
+            globalId++
+            return res.status(200).send(houses)
+        }
+    },
+    updateHouse: (req,res) => {
+        const {id} = req.params
+        const {type} = req.body
+        let index = houses.findIndex(elem => elem.id === +id)
+        if(houses[index].price === 0 && type === 'minus'){
+            res.status(400).send('Do you think money grows on trees?')
+        } else if(type === 'minus'){
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else if(type === 'plus'){
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else{
+            res.status(405)
+        }
+
+    }
+}
